1fab1e0b7633bd322151ac3c1485e67c213e8623
Karl-Weisenburger <karljweisenb@gmail.com>
2024-08-01 15:51:30 -0400

    New tests

--- wind_tomo/ARC.py
+++ wind_tomo/ARC.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
+from scipy.interpolate import interp1d
 
 
-def gen_wind_tunnel3(num_slices, num_rows, num_cols, left_freq=14, right_freq=16, center_rod=False):
+def gen_wind_tunnel3(num_slices, num_rows, num_cols, left_freq=14, right_freq=16, boundaries=True, center_rod=False):
     """
     Generate a phantom cheaply mimicking a wind tunnel
 
@@ -19,7 +20,7 @@ def gen_wind_tunnel3(num_slices, num_rows, num_cols, left_freq=14, right_freq=16
     radius = 0.07
     axis_x = np.linspace(-1, 1, num_cols)
     axis_y = np.linspace(1, -1, num_rows)
-    axis_z = np.linspace(radius+0.01, -radius-0.01, num_slices)
+    axis_z = np.linspace(1, -1, num_slices)
 
     y_grid,z_grid, x_grid = np.meshgrid(axis_y,axis_z, axis_x)
     image = x_grid * 0.0
@@ -31,26 +32,29 @@ def gen_wind_tunnel3(num_slices, num_rows, num_cols, left_freq=14, right_freq=16
     right_thick=0.015
     right_amp=0.1
     left_amp=0.05
-    #add left squiggle
-    image += ( ( (
-            x_grid <= right_amp * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid+right_thick) & (
-                      x_grid >= 0.1 * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid-right_thick) ) & ( (
-            z_grid <= right_amp * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid+right_thick) & (
-                      z_grid >= 0.1 * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid-right_thick) ) ) * 5
-    #add right squiggle
-    image += ( (
-            x_grid >= left_amp * np.sin(y_grid * 2 * np.pi * left_freq / 4) + left_mid - left_thick) & (
-                      x_grid <= 0.05 * np.sin(y_grid * 2 * np.pi * left_freq / 4) + left_mid + left_thick)) * 5
+    if boundaries:
+        #add left squiggle
+        image += ( (
+                x_grid <= right_amp * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid+right_thick) & (
+                          x_grid >= right_amp * np.sin(y_grid * 2 * np.pi * right_freq / 4) + right_mid-right_thick)
+                   #& (y_grid<=0.32)& (y_grid>=-0.3)
+                   )  * 3
+        #add right squiggle
+        image += ( (
+                x_grid >= left_amp * np.sin(y_grid * 2 * np.pi * left_freq / 4) + left_mid - left_thick) & (
+                          x_grid <= left_amp * np.sin(y_grid * 2 * np.pi * left_freq / 4) + left_mid + left_thick)
+                   #& (y_grid<=0.32)& (y_grid>=-0.3)
+                    ) * 3
     #add center rod
     if center_rod:
         image += (((x_grid<=0.01) & (x_grid>=-0.01)) & ((y_grid<=0.01) & (y_grid>=-0.01)))*3
 
-    xy_centers=[(-0.45,0.11),(-0.45,-0.1),(-0.25,0.12),(0.45,-0.05),(0.1,-0.15),(0.15,0.06),(0.35,0.11),(-0.15,-0.06)]
-    xy_distort=[(1,2),(2,1),(2,1),(3,1),(1,3),(4,1),(1,3),(1,4)]
+    xy_centers=[(-0.55,0.11,0),(-0.45,-0.1,-.1),(-0.25,0.12,.1),(0.6,-0.05,.1),(0.1,-0.15,0),(0.15,0.06,0),(0.45,0.11,.12),(-0.15,-0.06,-.1)]
+    xy_distort=[(1,2,0.1),(2,1,0.7),(2,1,0.1),(3,1,0.1),(1,3,0.8),(4,1,0.1),(1,3,0.2),(1,4,0.1)]
     value=[2,3,1,1,3,2,4,2]
 
     for i in range(len(xy_centers)):
-        circle=xy_distort[i][0]*(x_grid - xy_centers[i][0])**2 + xy_distort[i][1]*(y_grid - xy_centers[i][1])**2 + (z_grid)**2 - radius**2
+        circle=xy_distort[i][0]*(x_grid - xy_centers[i][0])**2 + xy_distort[i][1]*(y_grid - xy_centers[i][1])**2 + xy_distort[i][2]*(z_grid- xy_centers[i][2])**2 - radius**2
         image += np.where(circle<=0,value[i],0)
 
     return image
@@ -89,7 +93,7 @@ def windtunnel_block(sinogram,angles,num_rows,num_cols):
 
 def circ_block(view,diameter,center_offset=(0,0)):
     """
-    Set everything outside of a disk equal to zero
+    Set everything outside a disk equal to zero
 
     Args:
         view(ndarray): 2D array to be modified
@@ -106,9 +110,9 @@ def circ_block(view,diameter,center_offset=(0,0)):
     circ = (r<=diameter/2)*1
     return circ*view
 
-def multi_circ_block(view,diameter,num_stack=1,stack_offset=0, left_right_offset=0):
+def multi_circ_block(view,diameter,num_stack=1,stack_offset=0, left_right_offset=0,alpha=1,for_weights=False):
     """
-    Set everything outside of a stack of disks to zero
+    Set everything outside a stack of disks to zero
 
     Args:
         view(ndarray): 2D array to be modified
@@ -126,26 +130,34 @@ def multi_circ_block(view,diameter,num_stack=1,stack_offset=0, left_right_offset
     stack_positions = [j * stack_offset - (num_stack - 1) * stack_offset / 2 for j in range(num_stack)]
     for y_pos in stack_positions:
         center=(view.shape[0]//2+y_pos,view.shape[1]//2+left_right_offset)
-        r = np.sqrt((x-center[0])**2 + (y-center[1])**2)
+        r = np.sqrt((x-center[0])**2 + ((y-center[1])*alpha)**2)
         circ += (r<=diameter/2)*1
-    return view*(circ>0)
+    view=view * (circ > 0)
+    if not for_weights:
+        for i in range(H):
+            if np.sum(circ[i, :]) > 0:
+                left_ind = np.where(circ[i, :] > 0)[0][0]
+                right_ind = np.where(circ[i, :] > 0)[0][-1]
+                view[i, 0:left_ind] = view[i, left_ind]
+                view[i, right_ind + 1:] = view[i, right_ind]
+    return view
 
 
-def sino_window_and_circ_block(sinogram,angles,diameter,num_stack=1,stack_offset=0,center_offset=0):
+def sino_window_and_circ_block(sinogram,angles,recon_slice_dim,diameter,num_stack=1,stack_offset=0,center_offset=0,alpha_vals=1,for_weights=False):
     """
     Modify a sinogram to simulate CT with a stack of beams of a volume between two windows
 
     Args:
         sinogram(ndarray): 3D sinogram of shape (views, slices, channels)
         angles(ndarray): 1D numpy array of angles corresponding to the sinogram views
-        diameter(int): pixel diameter of the beams
+        diameter(ndarray or int): vector of integer pixel diameters for beams or one integer for all beam diameters
         num_stack(int): number of vertical stacks of disks
         stack_offset (int): pixel distance between the centers of the disks in the stack
         center_offset (int): pixel center of rotation along column axis. Allows user to place center closer to left or right side of recosntruction volume space.
     Return:
         newsinogram(ndarray): 3D sinogram where each view has been modified to account for the windows, and the placement of the beams
     """
-    num_rows, num_cols = sinogram[0].shape #(# of slices, # of channels)
+    num_rows, num_cols = recon_slice_dim #(# of slices, # of channels)
     #make copy
     newsino=sinogram.copy()
     num_channel=newsino.shape[2]
@@ -163,8 +175,12 @@ def sino_window_and_circ_block(sinogram,angles,diameter,num_stack=1,stack_offset
         oldsino=newsino.copy()
 
         # do beam circle thing
-        newsino[i,:,:]=multi_circ_block(oldsino[i,:,:],diameter,num_stack,stack_offset,center_offset*np.sin(-theta))
-
+        if np.size(alpha_vals)==1:
+            newsino[i,:,:]=multi_circ_block(oldsino[i,:,:],diameter,num_stack,stack_offset,center_offset*np.sin(-theta),for_weights=for_weights)
+        elif np.size(alpha_vals) == np.size(angles):
+            newsino[i,:,:]=multi_circ_block(oldsino[i,:,:],diameter,num_stack,stack_offset,center_offset*np.sin(-theta),alpha=alpha_vals[i],for_weights=for_weights)
+        else:
+            raise Exception("Input for alpha_vals must either be an integer or a vector with the same length as angles")
     return newsino
 
 def weights_window_and_circ_block(sinogram_shape,angles,diameter,num_stack=1,stack_offset=0,center_offset=0):
@@ -237,7 +253,7 @@ def JAX_sino_window_and_circ_block(sinogram,angles,diameters,window_dim):
 
     return newsino
 
-def grid_rescale_and_pad(view, compress_factor, upsample_factor=1, center_offset=0):
+def grid_rescale_and_pad(view, compress_factor, upsample_factor=1, center_offset=0,for_weights=False):
     """
     Resample a sinogram view along the channel axes. The resampling period is downsample_factor/upsample_factor. The
     final result is then zero padded to be upsample_factor times it's original length along the channel axis. Function
@@ -255,7 +271,10 @@ def grid_rescale_and_pad(view, compress_factor, upsample_factor=1, center_offset
     col_scale=compress_factor
     num_rows,num_cols = view.shape
     col_grid = np.linspace(0,num_cols-1,num_cols)
-    interp=CubicSpline(col_grid, view, axis=1, extrapolate=False)
+    if for_weights:
+        interp=interp1d(col_grid, view,kind="nearest-up", axis=1, bounds_error=False, fill_value=0)
+    else:
+        interp=CubicSpline(col_grid, view, axis=1, extrapolate=False)
     new_length=int(round(num_cols*upsample_factor))
     col_interpts=np.zeros(new_length)
     col_interpts[0]= ((num_cols-1)/2 + center_offset)*(1- col_scale)
@@ -266,7 +285,7 @@ def grid_rescale_and_pad(view, compress_factor, upsample_factor=1, center_offset
     return scaled_view
 
 
-def ARC_sino_transform(sino,angles,gamma,a=1,center_offset=0):
+def ARC_sino_transform(sino,angles,gamma,a=1,center_offset=0,for_weights=False):
     """
     Implements ARC sinogram transformation. Allows for the user to offset the resampling center of each sinogram view
 
@@ -281,17 +300,54 @@ def ARC_sino_transform(sino,angles,gamma,a=1,center_offset=0):
         sino_tilde(ndarray): 3D sinogram numpy array of shape (views, slices, channels*upsample_factor)
         new_angles(ndarray): 1D numpy array of transformed angles corresponding to the sinogram views
     """
-    num_channels=sino.shape[2] #
-    num_slices = sino.shape[1] #1
+    num_views, num_slices,num_channels=sino.shape
     new_channels=int(round(num_channels*a))
-    sino_tilde= np.zeros((sino.shape[0],num_slices,new_channels))
+    sino_tilde= np.zeros((num_views,num_slices,new_channels))
     new_angles=np.zeros(angles.shape)
     for thetaidx, theta in enumerate(angles):
         theta_tilde= np.arctan2(gamma*np.sin(theta),np.cos(theta))
         alpha= np.sqrt((gamma**2)*(np.sin(theta)**2)+np.cos(theta)**2)
-        sino_tilde[thetaidx,:,:]=(alpha/gamma)*grid_rescale_and_pad(sino[thetaidx,:,:],alpha,a,center_offset=center_offset)
+        if for_weights:
+            sino_tilde[thetaidx,:,:]=grid_rescale_and_pad(sino[thetaidx,:,:],alpha,a,
+                                                          center_offset=center_offset,
+                                                          for_weights=for_weights)
+        else:
+            sino_tilde[thetaidx, :, :] = (alpha / gamma) * grid_rescale_and_pad(sino[thetaidx, :, :], alpha, a,
+                                                                                center_offset=center_offset)
         new_angles[thetaidx]=theta_tilde
     return sino_tilde, new_angles
+
+def ARC_alpha_values(angles,gamma):
+    """
+    Implements ARC sinogram transformation. Allows for the user to offset the resampling center of each sinogram view
+
+    Args:
+        angles(ndarray): 1D numpy array of angles corresponding to the sinogram views
+        gamma(float): factor by which to compress the reconstruction along the axis aligned with the zero angle view
+    Return:
+        alpha_vals(ndarray): 1D numpy array of individual view channel compressions
+    """
+    alpha_vals=np.zeros(angles.shape)
+    for thetaidx, theta in enumerate(angles):
+        alpha= np.sqrt((gamma**2)*(np.sin(theta)**2)+np.cos(theta)**2)
+        alpha_vals[thetaidx]=alpha
+    return alpha_vals
+
+def ARC_angle_transform(angles,gamma):
+    """
+    Implements ARC sinogram transformation for the angles only
+
+    Args:
+        angles(ndarray): 1D numpy array of angles corresponding to the sinogram views
+        gamma(float): factor by which to compress the reconstruction along the axis aligned with the zero angle view
+    Return:
+        new_angles(ndarray): 1D numpy array of transformed angles corresponding to the sinogram views
+    """
+    new_angles=np.zeros(angles.shape)
+    for thetaidx, theta in enumerate(angles):
+        theta_tilde= np.arctan2(gamma*np.sin(theta),np.cos(theta))
+        new_angles[thetaidx]=theta_tilde
+    return new_angles
 
 def find_diameters(angles,dist,thick):
     return [(dist*np.sin(abs(theta)) + thick*np.cos(abs(theta))) for theta in angles]
